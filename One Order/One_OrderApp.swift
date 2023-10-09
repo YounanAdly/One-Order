badcc7e7e776c459d015d9950f2475f0a173c989
Younan Adly <42420496+YounanAdly@users.noreply.github.com>
2023-10-09 21:22:15 +0300

    Initial Commit

---
+++ One Order/One_OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_OrderApp.swift
+//  One Order
+//
+//  Created by younan on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct One_OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
